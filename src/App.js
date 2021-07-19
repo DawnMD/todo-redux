b0bc36f64c2db5e79e227c76080b0443ea4096fe
Mainak Das <mainakdas104@gmail.com>
2021-07-19 15:05:15 +0530

    added ui components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React, { Component } from "react";
-import { addCard,removeCard} from './actions'
-import { connect } from "react-redux";
-import { Field, reduxForm } from 'redux-form'
-import { compose} from 'redux'
+import React, { Component } from 'react';
+import { addCard, removeCard } from './actions';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { compose } from 'redux';
+import Button from './components/Shared/UI/Button';
+import Input from './components/Shared/UI/Input';
+import Label from './components/Shared/UI/Label';
+import Card from './components/Card';
 
 class App extends Component {
-  addCardHandler = (formProps) => {
-    this.props.reset()
-    this.props.addCard(formProps.card)
-  }
-  render() {
-    console.log(this.props)
-    return (
-      <div >
-        <form onSubmit={ this.props.handleSubmit(this.addCardHandler) }>
-          <label htmlFor='card'>Enter Todo List Name</label>
-          <Field name="card" component="input" type="text" placeholder='Name'/>
-          <button>Add</button>
-        </form>
-        <div>
-          <h2>Todo Cards</h2>
-          {this.props.cardWithItems.card && this.props.cardWithItems.card.map(cardName => <div key={ cardName}>{ cardName}</div>)}
-        </div>
-        
-      </div>
-    );
-  }
+	addCardHandler = (formProps) => {
+		this.props.reset();
+		this.props.addCard(formProps.card);
+	};
+	render() {
+		return (
+			<div className='flex flex-col gap-6'>
+				<form
+					onSubmit={this.props.handleSubmit(this.addCardHandler)}
+					className='flex items-center justify-center gap-3'>
+					<Label htmlFor='card'>Enter card name</Label>
+					<Input name='card' placeholder='Card Name' />
+					<Button>Add Card</Button>
+				</form>
+				<div className='flex flex-col items-center gap-6'>
+					<h2 className='text-2xl font-semibold text-center'>TODO Cards</h2>
+					<div className='flex flex-col gap-4'>
+						{this.props.cards &&
+							this.props.cards.map((card) => (
+								<Card cardName={card} key={card} />
+							))}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-  return { cardWithItems:state}
-}
+	return { cards: state.card, state };
+};
 
 export default compose(
-  connect(mapStateToProps, { addCard,removeCard }),
-  reduxForm({
-    form: "card",
-  })
+	connect(mapStateToProps, { addCard, removeCard }),
+	reduxForm({
+		form: 'addCard',
+	})
 )(App);
--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,12 +1,12 @@
-import { ADD_ITEM_TO_CARD,REMOVE_ITEM_FROM_CARD } from "../actions/constants";
+import { ADD_ITEM_TO_CARD, REMOVE_ITEM_FROM_CARD } from '../actions/constants';
 const addItem = (state = [], action) => {
-  switch (action.type) {
-    case ADD_ITEM_TO_CARD:
-      return [...state, action.payload];
-    case REMOVE_ITEM_FROM_CARD:
-      return state.filter((item) => item !== action.payload);
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case ADD_ITEM_TO_CARD:
+			return [...state, action.payload];
+		case REMOVE_ITEM_FROM_CARD:
+			return state.filter((item) => item !== action.payload);
+		default:
+			return state;
+	}
 };
 export default addItem;
